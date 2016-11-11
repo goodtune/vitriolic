ed92332d06c3f74eca378abea68fb89936282c54
Gary Reynolds <gary@touch.asn.au>
2016-11-11 22:28:22 +1100

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='vitriolic',
+    version='1.0.0',
+    author='Gary Reynolds',
+    author_email='gary@touch.asn.au',
+    packages=find_packages(),
+    zip_safe=False,
+)
