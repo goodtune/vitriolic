233deec9f7102cb85fe873f71129c50068d8d759
Gary Reynolds <gary@touch.asn.au>
2016-12-08 07:33:39 +1100

    Update setup.py to provide a description and long_description
    - added README.md to MANIFEST.in so it gets bundled in the source
      distribution
    - hopefully I've protected myself from failure in the above by wrapping
      the ingestion of README.md in a try/except block

--- setup.py
+++ setup.py
@@ -1,5 +1,10 @@
 from setuptools import find_packages, setup
 
+try:
+    with open('README.md') as fp:
+        long_description = fp.read()
+except IOError:
+    long_description = None
 
 setup(
     name='vitriolic',
@@ -7,6 +12,12 @@ setup(
     author='Gary Reynolds',
     author_email='gary@touch.asn.au',
     license='BSD',
+    description=(
+        'A suite of packages for the Django framework to provide integrated '
+        'Content Management and Sports Administration functionality for '
+        'sporting associations.'
+    ),
+    long_description=long_description,
     packages=find_packages(),
     install_requires=[
         'django-classy-tags~=0.7.2',
