b6c05217a585452f1ddc65080a1bbd2cdcbf2abc
Gary Reynolds <gary@touch.asn.au>
2018-05-15 07:57:49 +1000

    Fix HTTP 500 errors when Article had multiple categories.

--- touchtechnology/news/tests/test_site.py
+++ touchtechnology/news/tests/test_site.py
@@ -1,5 +1,7 @@
+from django.db import transaction
 from django.test.utils import override_settings
 from test_plus import TestCase
+from touchtechnology.news.tests import factories
 
 
 @override_settings(ROOT_URLCONF='example_app.urls')
@@ -23,3 +25,41 @@ class FeedTest(TestCase):
 
     def test_rss(self):
         self.assertGoodView('news:feed', format='rss')
+
+
+@override_settings(ROOT_URLCONF='example_app.urls')
+class SiteTest(TestCase):
+
+    def assertGoodArticleView(self, article):
+        self.assertGoodView(
+            'news:article',
+            year=article.published.year,
+            month=article.published.strftime('%b').lower(),
+            day=article.published.day,
+            slug=article.slug)
+
+    def test_article(self):
+        article = factories.ArticleFactory.create(
+            headline='This is a predictable headline!')
+        self.assertEqual(article.slug, 'this-is-a-predictable-headline')
+        self.assertGoodArticleView(article)
+
+    def test_article_one_category(self):
+        categories = factories.CategoryFactory.create_batch(1)
+        article = factories.ArticleFactory.create()
+        article.categories.set(categories)
+        self.assertGoodArticleView(article)
+
+    def test_article_many_categories(self):
+        categories = factories.CategoryFactory.create_batch(3)
+        article = factories.ArticleFactory.create()
+        article.categories.set(categories)
+        self.assertGoodArticleView(article)
+
+    def test_multiple_articles_related_categories(self):
+        with transaction.atomic():
+            categories = factories.CategoryFactory.create_batch(3)
+            articles = factories.ArticleFactory.create_batch(2)
+            for article in articles:
+                article.categories.set(categories)
+        self.assertGoodArticleView(article)
