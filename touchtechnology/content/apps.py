e9201f6e23fb6ba96376032ba9acd6a31686de49
Gary Reynolds <gary@touch.asn.au>
2019-01-14 23:40:58 +1100

    Fix up touchtechnology.content install_placeholder as a post_migrate handler.

--- touchtechnology/content/apps.py
+++ touchtechnology/content/apps.py
@@ -2,14 +2,13 @@ from django.apps import AppConfig
 from django.db.models.signals import post_migrate
 
 
-def install_placeholder_handler(sender, apps, **kwargs):
+def install_placeholder_handler(sender, **kwargs):
     from touchtechnology.content.utils import install_placeholder
-    # for config in apps.get_app_configs():
-    #     install_placeholder(config.name)
+    install_placeholder(sender.name)
 
 
 class ContentConfig(AppConfig):
     name = "touchtechnology.content"
 
     def ready(self):
-        post_migrate.connect(install_placeholder_handler, sender=self)
+        post_migrate.connect(install_placeholder_handler)
