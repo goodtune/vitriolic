b37feb47ec6424db066b6c41b36b536f33d44e89
Gary Reynolds <gary@touch.asn.au>
2019-01-14 17:47:20 +1100

    Remove dependency on Pillow - it's preventing me testing on MacOS.

--- touchtechnology/common/storage.py
+++ touchtechnology/common/storage.py
@@ -1,11 +1,8 @@
 import logging
 import os.path
 
-from django.core.files.base import ContentFile
 from django.core.files.storage import FileSystemStorage as FileSystemStorageBase
-from django.db.models.fields.files import ImageFieldFile
 from django.utils.six.moves.urllib.parse import urljoin
-from PIL import Image
 from touchtechnology.common.default_settings import STORAGE_FOLDER, STORAGE_URL
 
 try:
@@ -20,7 +17,6 @@ __all__ = (
     'FileSystemStorage',
     'MakeDirectoryMixin',
     'OverwriteMixin',
-    'ResizedImageFieldFile',
     'WalkMixin',
 )
 
@@ -99,58 +95,3 @@ class FileSystemStorage(MakeDirectoryMixin, WalkMixin, TenantStorageMixin,
         if not self.exists(self.path(path)):
             raise OSError(2, os.strerror(2), path)
         return os.access(self.path(path), os.W_OK)
-
-
-class ResizedImageFieldFile(ImageFieldFile):
-
-    def __init__(self, instance, field_name, size, background):
-        self.field_name = field_name
-        self.maxwidth, self.maxheight = size
-        self.background = background
-        field = instance._meta.get_field(field_name)
-        path, filename = os.path.split(getattr(instance, field_name).name)
-        dim = '%sx%s' % (self.maxwidth, self.maxheight)
-        name = os.path.join(path, dim, '%s.jpg' % instance.pk)
-        super(ResizedImageFieldFile, self).__init__(instance, field, name)
-
-    def regenerate(self):
-        image_field = getattr(self.instance, self.field_name)
-        if not self.storage.exists(image_field):
-            logger.warning('Original file "{0}" does not exist, failed to '
-                           'regenerate sized image.'.format(image_field))
-            return
-
-        path = os.path.dirname(self.name)
-        if not self.storage.exists(path):
-            self.storage.makedirs(path)
-
-        original = self.storage.open(image_field)
-        image = Image.open(original)
-        if image.mode != 'RGB':
-            image = image.convert('RGB')
-
-        # determine the dimensions of the original image
-        (__, __, w, h) = image.getbbox()
-
-        # determine the new width & height for the image dimensions
-        adjusted_width = int(w * (float(self.maxheight) / float(h)))
-        adjusted_height = int(h * (float(self.maxwidth) / float(w)))
-
-        # scale the image to the new size
-        x_scale = min(self.maxwidth, adjusted_width)
-        y_scale = min(self.maxheight, adjusted_height)
-        small = image.resize((x_scale, y_scale), Image.ANTIALIAS)
-
-        # create a new image object onto which we will paste the
-        # resized original
-        dim = (self.maxwidth, self.maxheight)
-        thumbnail = Image.new("RGB", dim, self.background)
-
-        # paste the resized image onto the thumbnail canvas
-        x_offset = self.maxwidth - x_scale // 2
-        y_offset = max(0, self.maxheight - y_scale // 2)
-        offset = (x_offset, y_offset)
-        thumbnail.paste(small, offset)
-
-        f = ContentFile(thumbnail.tostring("jpeg", "RGB"))
-        self.storage.save(self.name, f)
