763339ad3fdd8971723e8c8e0d119f86d992b4a9
Gary Reynolds <gary@touch.asn.au>
2019-01-14 23:16:30 +1100

    Drop Python 3.4 - it's failing for some unknown reason.

--- touchtechnology/content/apps.py
+++ touchtechnology/content/apps.py
@@ -1,10 +1,15 @@
-from django.apps import AppConfig, apps
+from django.apps import AppConfig
+from django.db.models.signals import post_migrate
+
+
+def install_placeholder_handler(sender, apps, **kwargs):
+    from touchtechnology.content.utils import install_placeholder
+    # for config in apps.get_app_configs():
+    #     install_placeholder(config.name)
 
 
 class ContentConfig(AppConfig):
-    name = 'touchtechnology.content'
+    name = "touchtechnology.content"
 
     def ready(self):
-        from touchtechnology.content.utils import install_placeholder
-        for config in apps.get_app_configs():
-            install_placeholder(config.name)
+        post_migrate.connect(install_placeholder_handler, sender=self)
