7533e4afb6ce42dee0d1208b1528b298cd53fae2
Gary Reynolds <gary.reynolds@touchtechnology.com.au>
2019-12-17 22:13:40 +1100

    Chunk.copy has to be treated as safe.

--- touchtechnology/content/templatetags/content.py
+++ touchtechnology/content/templatetags/content.py
@@ -1,6 +1,7 @@
 import logging
 
 from django.template import Library
+from django.utils.safestring import mark_safe
 from touchtechnology.content.models import Chunk
 
 logger = logging.getLogger(__name__)
@@ -11,5 +12,5 @@ register = Library()
 def chunk(slug):
     instance, created = Chunk.objects.get_or_create(slug=slug)
     if created:
-        logger.debug('Created %r' % instance)
-    return instance.copy
+        logger.debug("Created %r" % instance)
+    return mark_safe(instance.copy)
