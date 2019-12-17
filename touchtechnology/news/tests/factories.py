7e2eb7e5f88e88e61c3b87d14e17040f6a8574de
Gary Reynolds <gary.reynolds@touchtechnology.com.au>
2019-12-17 22:10:06 +1100

    Add unit test to ensure links back-and-forth for Article and Translation

--- touchtechnology/news/tests/factories.py
+++ touchtechnology/news/tests/factories.py
@@ -4,23 +4,33 @@ import factory
 import factory.fuzzy
 from django.utils import timezone
 from django.utils.text import slugify
-from touchtechnology.news.models import Article, Category
+from touchtechnology.news.models import Article, Category, Translation
 
 
 class ArticleFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Article
 
-    headline = factory.Faker('sentence')
-    abstract = factory.Faker('paragraph')
+    headline = factory.Faker("sentence")
+    abstract = factory.Faker("paragraph")
     published = factory.fuzzy.FuzzyDateTime(
-        datetime.datetime(2013, 7, 15, tzinfo=timezone.utc),
-        force_microsecond=0)
+        datetime.datetime(2013, 7, 15, tzinfo=timezone.utc), force_microsecond=0
+    )
+    copy = factory.Faker("paragraph")
     slug = factory.LazyAttribute(lambda a: slugify(a.headline))
+
+
+class TranslationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Translation
+
+    headline = factory.Faker("sentence")
+    abstract = factory.Faker("paragraph")
+    copy = factory.Faker("paragraph")
 
 
 class CategoryFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Category
 
-    title = factory.Faker('country')
+    title = factory.Faker("country")
