209328b20c83e26420d952a3e8fcb9f6f6cd1fd6
Gary Reynolds <gary.reynolds@touchtechnology.com.au>
2019-12-19 11:39:47 +1100

    Clean up touchtechnology.news.migrations

--- touchtechnology/news/migrations/0002_article_last_modified.py
+++ touchtechnology/news/migrations/0002_article_last_modified.py
@@ -1,24 +1,27 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
 import datetime
 
-import touchtechnology.common.db.models
 from django.db import migrations
 from django.utils.timezone import utc
+
+import touchtechnology.common.db.models
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('news', '0001_initial'),
+        ("news", "0001_initial"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='article',
-            name='last_modified',
-            field=touchtechnology.common.db.models.DateTimeField(default=datetime.datetime(2015, 3, 23, 11, 10, 25, 290897, tzinfo=utc), auto_now=True),
+            model_name="article",
+            name="last_modified",
+            field=touchtechnology.common.db.models.DateTimeField(
+                default=datetime.datetime(2015, 3, 23, 11, 10, 25, 290897, tzinfo=utc),
+                auto_now=True,
+            ),
             preserve_default=False,
         ),
     ]
