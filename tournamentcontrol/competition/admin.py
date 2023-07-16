6d719a702c7d2c31312c7c3002bdcd1d00e57524
Gary Reynolds <gary@touch.asn.au>
2023-07-16 14:10:54 +1000

    feature: set thumbnail when creating match

--- tournamentcontrol/competition/admin.py
+++ tournamentcontrol/competition/admin.py
@@ -1597,10 +1597,17 @@ class CompetitionAdminComponent(CompetitionAdminMixin, AdminComponent):
                 elif obj.live_stream:
                     broadcast = (
                         season.youtube.liveBroadcasts()
-                        .insert(part="id,snippet,status,contentDetails", body=body)
+                        .insert(
+                            part="id,snippet,status,contentDetails",
+                            body=body,
+                        )
                         .execute()
                     )
                     obj.external_identifier = broadcast["id"]
+                    season.youtube.thumbnails().set(
+                        videoId=obj.external_identifier,
+                        media_body=obj.live_stream_thumbnail_media_body,
+                    ).execute()
                     video_link = f"https://youtu.be/{obj.external_identifier}"
                     obj.videos = (
                         [video_link]
