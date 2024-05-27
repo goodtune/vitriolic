422c64889f5c93c4e69661862909d156db320cd4
Gary Reynolds <gary@touch.asn.au>
2024-05-27 16:46:47 -0500

    chore: `plpgsql` function `vitriolic_stage_group_position`
    This function mimicks the code in [`Match._get_team`][1] for translating
    strings such as `P1` to `1st`, `G1P2` to `2nd Group 1`, or `S1G2P3` to
    `3rd Stage 1 Group 2`, allowing it to be moved into the database.
    
    [1]: https://github.com/goodtune/vitriolic/blob/11df430223fac1d24d01083a462b0bbecc6d1754/tournamentcontrol/competition/models.py#L1856-L1905

--- tournamentcontrol/competition/utils.py
+++ tournamentcontrol/competition/utils.py
@@ -10,7 +10,7 @@ from typing import Union
 import pytz
 from django.apps import apps
 from django.contrib.auth import get_user_model
-from django.db.models import F, Q
+from django.db.models import F, Func, Q
 from django.http import HttpResponse
 from django.template.loader import select_template
 from django.utils import timezone
@@ -59,6 +59,10 @@ stage_group_position_re = re.compile(
     """,
     re.VERBOSE,
 )
+
+
+class StageGroupPosition(Func):
+    function = "vitriolic_stage_group_position"
 
 
 class FauxQueryset(list):
