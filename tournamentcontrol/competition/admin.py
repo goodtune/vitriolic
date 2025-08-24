de7a80e2418b685419968249d9f97d14e85c8f02
Gary Reynolds <gary@touch.asn.au>
2025-08-24 19:35:22 +1000

    Reformatted with `uv format`

--- tournamentcontrol/competition/admin.py
+++ tournamentcontrol/competition/admin.py
@@ -13,7 +13,10 @@ from django.db import models
 from django.db.models import Case, F, Q, Sum, When
 from django.forms.models import _get_foreign_key
 from django.http import (
-    Http404, HttpResponse, HttpResponseGone, HttpResponseRedirect,
+    Http404,
+    HttpResponse,
+    HttpResponseGone,
+    HttpResponseRedirect,
 )
 from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
@@ -24,15 +27,20 @@ from googleapiclient.errors import HttpError
 
 from touchtechnology.admin.base import AdminComponent
 from touchtechnology.common.decorators import (
-    csrf_exempt_m, staff_login_required_m,
+    csrf_exempt_m,
+    staff_login_required_m,
 )
 from touchtechnology.common.prince import prince
 from tournamentcontrol.competition.dashboard import (
-    BasicResultWidget, DetailResultWidget, MostValuableWidget,
-    ProgressStageWidget, ScoresheetWidget,
+    BasicResultWidget,
+    DetailResultWidget,
+    MostValuableWidget,
+    ProgressStageWidget,
+    ScoresheetWidget,
 )
 from tournamentcontrol.competition.decorators import (
-    competition_by_pk_m, registration,
+    competition_by_pk_m,
+    registration,
 )
 from tournamentcontrol.competition.forms import (
     ClubAssociationForm,
@@ -65,19 +73,45 @@ from tournamentcontrol.competition.forms import (
     VenueForm,
 )
 from tournamentcontrol.competition.models import (
-    Club, ClubAssociation, ClubRole, Competition, Division,
-    DivisionExclusionDate, DrawFormat, Ground, LadderEntry, LadderSummary,
-    Match, MatchScoreSheet, Person, Place, Season, SeasonAssociation,
-    SeasonExclusionDate, SeasonMatchTime, SeasonReferee, Stage, StageGroup,
-    Team, TeamAssociation, TeamRole, UndecidedTeam, Venue,
+    Club,
+    ClubAssociation,
+    ClubRole,
+    Competition,
+    Division,
+    DivisionExclusionDate,
+    DrawFormat,
+    Ground,
+    LadderEntry,
+    LadderSummary,
+    Match,
+    MatchScoreSheet,
+    Person,
+    Place,
+    Season,
+    SeasonAssociation,
+    SeasonExclusionDate,
+    SeasonMatchTime,
+    SeasonReferee,
+    Stage,
+    StageGroup,
+    Team,
+    TeamAssociation,
+    TeamRole,
+    UndecidedTeam,
+    Venue,
 )
 from tournamentcontrol.competition.sites import CompetitionAdminMixin
 from tournamentcontrol.competition.tasks import (
-    generate_pdf_grid, generate_pdf_scorecards, set_youtube_thumbnail,
+    generate_pdf_grid,
+    generate_pdf_scorecards,
+    set_youtube_thumbnail,
 )
 from tournamentcontrol.competition.utils import (
-    FauxQueryset, generate_fixture_grid, generate_scorecards,
-    legitimate_bye_match, match_unplayed,
+    FauxQueryset,
+    generate_fixture_grid,
+    generate_scorecards,
+    legitimate_bye_match,
+    match_unplayed,
 )
 from tournamentcontrol.competition.wizards import DrawGenerationWizard
 
