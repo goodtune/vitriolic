a499bc8f741423b6a01511b5f9e565169400fdd9
Gary Reynolds <gary@touch.asn.au>
2025-09-26 20:58:39 +1000

    Rename migration file to resolve migration conflicts

--- tournamentcontrol/competition/migrations/0055_remove_ranking_fields.py
+++ tournamentcontrol/competition/migrations/0055_remove_ranking_fields.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("competition", "0052_add_club_status"),
+        ("competition", "0054_migrate_thumbnail_urls_to_binary"),
     ]
 
     operations = [
